df95cffef7f2b03e427d7b2806a13f10cf9036b4
rubens Rodrigues <rubens@schoolguardian.app>
2022-09-30 23:54:28 -0300

    Section 2 - Props

---
+++ src/Components/CustomLabel/CustomLabel.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CustomLabel.css";
+
+const CustomLabel = ({ info }) => {
+  return <div className="textClass">{info}</div>;
+};
+
+export default CustomLabel;
---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CustomLabel from "../../Components/CustomLabel/CustomLabel";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CustomLabel info={"Info Rubens"} />
+    </div>
+  );
+};
+
+export default Home;
